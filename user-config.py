668348ca450cbab92e46f806b9bc5c7ba9898601
Paul BOUAFFOU <43551198+paulbouaffou@users.noreply.github.com>
2025-03-30 06:51:08 +0000

    Update user-config.py

--- user-config.py
+++ user-config.py
@@ -1,1 +1,19 @@
+# Configuration spécifique de Pywikibot pour Wikidata
+
+mylang = "wikidata"
+family = "wikidata"
+usernames = {
+    'wikidata': {
+        'wikidata': 'ArchinvingBot'
+    }
+}
+
+default_edit_summary = "Automatic addition of an archived URL via Wikiwix"
+
+# Activation de l'API pour les modifications
+enable_api = True
+
+# Configuration du délai entre les éditions pour éviter de surcharger les serveurs
+put_throttle = 10
+
 
